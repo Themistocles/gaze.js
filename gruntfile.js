caba09f73a5f8e94162241f69fb1d0f1e6d4f82a
Ralf Biedert <rb@xr.io>
2014-04-05 23:47:20 +0200

    Gruntified project.

---
+++ gruntfile.js
@@ -0,0 +1,82 @@
+var version = "0.6.1";
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Compile coffeescript
+        coffee: {
+            compileWithMaps: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                'gaze.js': 'gaze.coffee', // 1:1 compile
+                }
+            }
+        },
+
+
+        // Minify
+        uglify: {
+            options: {
+                banner: '/*! http://gaze.io - <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+
+            build: {
+                src: 'gaze.js',
+                dest: 'gaze.min.js'
+            }
+        },
+
+
+        // Prepare dist
+        copy: {
+            main: {
+                files: [
+                    { expand: true, src: ['gaze.js'], dest: 'dist/', filter: 'isFile' },
+
+                    {
+                        expand: true,
+                        src: ['gaze.js'],
+                        dest: 'dist/' ,
+                        filter: 'isFile',
+                        rename: function(dest, src) {
+                            return dest + src.replace(/gaze\.js/, "gaze-" + version + ".js");
+                        }
+                    }
+                ]
+            }
+        },
+
+
+        'ftp-deploy': {
+            build: {
+                auth: {
+                    host: 'gaze.io',
+                    port: 21,
+                    authKey: 'xr'
+                },
+
+                src: 'dist/',
+                dest: '/www.gazeio.downloads/gaze.io',
+                //exclusions: ['./.*', './node_modules/' ]
+            },
+        },
+
+  });
+
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-ftp-deploy');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+
+  // Default task(s).
+  grunt.registerTask('default', ['coffee', 'uglify', 'copy', 'ftp-deploy']);
+
+};
